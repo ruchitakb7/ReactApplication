3a18a9f1ce1f4098cd8a5e63381ed2f8136feb58
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-30 18:12:07 +0530

    Button component

--- my-app/src/components/UI/Card.js
+++ my-app/src/components/UI/Card.js
@@ -1,10 +1,9 @@
 
-
+import React from "react";
 import "./Card.css";
 
-function Card(props) {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-}
+const Card = (props) => {
+  return <div className={`card ${props.className}`}>{props.children}</div>;
+};
 
 export default Card;
--- my-app/src/components/Users/AddUser.js
+++ my-app/src/components/Users/AddUser.js
@@ -2,6 +2,7 @@
 import React from "react";
 import "./AddUser.css"
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = () => {
   const addUserHandler = (event) => {
@@ -9,16 +10,17 @@ const AddUser = () => {
   };
 
   return (
-    <Card>
-    <form  className="input" onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className="input">
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
   );
+    
 };
 
 export default AddUser;
