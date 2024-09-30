2ac0a4732f20c222be9f480fdae7f41e94f08108
ruchitakb7 <ruchitakb7@gmail.com>
2024-09-30 18:34:30 +0530

    collecting input feild

--- my-app/src/components/UI/Button.js
+++ my-app/src/components/UI/Button.js
@@ -3,7 +3,8 @@ import "./Button.css";
 
 const Button = (props) => {
   return (
-    <button className="button" type={props.type || "button"}>
+    <button className="button" type={props.type || "button"}
+    onClick={props.onClick}>
       {props.children}
     </button>
   );
--- my-app/src/components/Users/AddUser.js
+++ my-app/src/components/Users/AddUser.js
@@ -1,21 +1,41 @@
 
-import React from "react";
+import React, {useState} from "react";
 import "./AddUser.css"
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const AddUser = () => {
+  const [username,setUsername]=useState('') 
+  const [age,setAge]=useState('') 
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(username,age)
+    setUsername('')
+    setAge('')
   };
+
+  const usernameHandler=(event)=>{
+    setUsername(event.target.value)
+    
+  }
+
+  const ageHandler=(event)=>{
+    setAge(event.target.value)
+  }
 
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text"
+        value={username}
+        onChange={usernameHandler} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number"
+        value={age}
+        onChange={ageHandler}
+         />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
